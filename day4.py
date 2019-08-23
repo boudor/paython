790ed2d07dae8ed1142ba2afad3a770398b00ec4
boudor <52617103+boudor@users.noreply.github.com>
2019-08-23 22:37:57 +0300

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,18 @@
+s = 3
+print(s)
+g = 'dont give up'
+print(g)
+w = 'aowese'
+print("paython +x")
+p = "bodour"
+t = "saleem"
+z = p + t 
+print(z)
+n = r = i ="orange"
+print (n)
+print(r)
+print (i)
+l = 3
+g = 5 
+print(l + g )
+
