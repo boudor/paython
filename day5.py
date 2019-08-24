c2adb33fba4a1ddac0421159cb377212f7db0b1b
boudor <52617103+boudor@users.noreply.github.com>
2019-08-24 11:55:16 +0300

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,28 @@
+p = 5
+u = 4.2 
+t = 1j 
+print(type(p))
+print(type(u))
+print(type(t))
+x = 6 
+y = 5.8 
+z = 4j 
+b = float (x) 
+o = int (y) 
+m = complex (z)
+print(b)
+print(o)
+print(m)
+print(type(b))
+print(type(o))
+print(type(m))
+import random
+print(random.randrange(1,10))
+
+
+
+
+
+
+
+
