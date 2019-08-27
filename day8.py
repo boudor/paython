f0367d15cbd254793749d09a606c18944a4bc9b3
boudor <52617103+boudor@users.noreply.github.com>
2019-08-27 08:53:04 +0300

    Add files via upload

---
+++ day8.py
@@ -0,0 +1,10 @@
+a ="no more fries"
+print (a)
+x = """What happens here
+Do you accept me as a friend for you
+Have you heard anything new"""
+print (x) 
+m = "frinds"
+print(m[2])
+q = "naught,volley"
+print (q[3:9])
