f4a02e868ed7f6171c39d8b65842088dc8218c77
boudor <52617103+boudor@users.noreply.github.com>
2019-08-27 08:53:40 +0300

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,10 @@
+a = " Be , careful! "
+print (a.strip())
+z = "welcome to harmash"
+print(len(z))
+o = "HI EVERY ONE"
+print(o.lower())
+print(o.upper())
+m = "hi,nothing"
+print(m.replace("h","o"))
+print(m.split(","))
