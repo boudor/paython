cf24ac3dbeae847df9feefe15b0123ff164f3ae1
boudor <52617103+boudor@users.noreply.github.com>
2019-08-31 22:47:43 +0300

    Delete day13.py

--- day13.py
+++
@@ -1,7 +0,0 @@
-x = "Please, I want {} sandwishes and {} donutes"
-print(x.replace("I","we"))
-tee = 5
-bb= 7
-z = "Please, I want {} sandwishes and {} donutes"
-print(z.format(tee,bb))
-print(z.upper())
