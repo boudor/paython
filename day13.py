71f0ec4a8f52e578d6f10684c8786dd6e6ac8106
boudor <52617103+boudor@users.noreply.github.com>
2019-08-31 22:52:57 +0300

    Add files via upload

---
+++ day13.py
@@ -0,0 +1,7 @@
+x = "Please, I want {} sandwishes and {} donutes"
+print(x.replace("I","we"))
+tee = 5
+bb= 7
+z = "Please, I want {} sandwishes and {} donutes"
+print(z.format(tee,bb))
+print(z.replace("a","A"))
