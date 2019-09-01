607e93a8082f5eb267cb62caf96d47f3d9d1f2a4
boudor <52617103+boudor@users.noreply.github.com>
2019-09-01 22:32:37 +0300

    Add files via upload

---
+++ day14.py
@@ -0,0 +1,19 @@
+s = []
+print(s)
+numbers = [1,2,3,4,5]
+print(numbers)
+thislist =["apple","banan","cheryy"]
+print(thislist)
+thislist =["apple","banan","cheryy", 6 , 8 , 9]
+print(thislist)
+thislist =["apple","banan","cheryy"]
+print(thislist[1])
+thislist = ["apple","banan","cheryy"]
+for x in thislist :
+ print (x)
+ thislist = ["apple","banan","cheryy"]
+ thislist[1] = "blackcurrant"
+ print (thislist)
+thislist = ["apple","banan","cheryy"]
+del thislist[1]
+print(thislist)
