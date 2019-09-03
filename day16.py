54e256f7caaa94c95f96d4367b33bce1c2f089d2
boudor <52617103+boudor@users.noreply.github.com>
2019-09-03 14:38:31 +0300

    Add files via upload

---
+++ day16.py
@@ -0,0 +1,17 @@
+thislist = ["i","e","q","o"]
+print(len(thislist))
+thislist = ["hhh","jjj"]
+thislist.append("ppp")
+print(thislist)
+thislist= ["apple","banan","orang"]
+thislist.insert(1,"cheryy")
+print(thislist)
+thislist.remove("apple")
+print(thislist)
+thislist.pop()
+print(thislist)
+thislist.clear()
+print(thislist)
+thislist = ["o","a","q"]
+mylist=thislist.copy()
+print(mylist)
