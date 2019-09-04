79ee4dba103842d418d0e4a8459126d2eb677e8e
boudor <52617103+boudor@users.noreply.github.com>
2019-09-04 15:57:31 +0300

    Add files via upload

---
+++ day17.py
@@ -0,0 +1,16 @@
+thistuple = ("apple","banan","cherry")
+print(thistuple)
+thistuple = ()
+print (thistuple)
+thistuple = (3,)
+print (thistuple)
+thistuple = ("bodour","saleem","mohamed" , 9.8,3)
+print (thistuple)
+print(thistuple[3])
+thistuple = ("bodour","saleem","mohamed" , 9.8,3)
+for x in thistuple:
+ print(x)
+x = ("apple","happy","house","garden")
+print(x [0:2])
+
+
