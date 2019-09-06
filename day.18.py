f4b888052ddddde89560cecf53efba353dad2e2d
boudor <52617103+boudor@users.noreply.github.com>
2019-09-06 23:15:11 +0300

    Add files via upload

---
+++ day.18.py
@@ -0,0 +1,20 @@
+thistuple = ["apple","panan","happy"]
+thistuple.append("hh")
+print(thistuple)
+g = ["h","h","a"]
+g.insert(1,"sad")
+print(g)
+z = ["h","t","a"]
+z.pop
+print(z)
+i = ["h","d","z"]
+i.remove("h")
+print(i)
+i.clear()
+print(i)
+thistuple = ["hi","welcome","cheryy"]
+mylist = thistuple.copy()
+print(mylist)
+k = ("jave","c++","python")
+if "python" in k:
+ print("yes it is")
