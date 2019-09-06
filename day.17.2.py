4e40df1a6687a0f743833a99b5a5ee70e6f9e065
boudor <52617103+boudor@users.noreply.github.com>
2019-09-06 14:10:53 +0300

    Add files via upload

---
+++ day.17.2.py
@@ -0,0 +1,15 @@
+thistuple = ("mohamed","ahmed","sarh")
+if "mohamed" in thistuple:
+ print("yes,he is here")
+ thistuple = ("bodour amizing girl,")*4
+ print(thistuple)
+ x = (1,3,5,5)
+ q = (9,6,6)
+ v = x+q
+ print(v)
+ print (len(v))
+i = (("hi","wlcome","sir"))
+print(i)
+thistuple =[2,4,4,"a","e"]
+thistuple = tuple(thistuple)
+print(thistuple)
