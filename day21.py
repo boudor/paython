dbe2dde42a83d47839b1a4aeee4c7906b5204039
boudor <52617103+boudor@users.noreply.github.com>
2019-09-08 17:53:51 +0300

    Add files via upload

---
+++ day21.py
@@ -0,0 +1,11 @@
+thisset = {"she","is","samrt"} 
+print(len(thisset))
+thisset.remove("is")
+say = {"apple","banan","jojo","welcome"}
+x = say.pop()
+print(x)
+print(say)
+say.clear()
+print(say)
+i = (("hi","hi","hi"))
+print(i)
