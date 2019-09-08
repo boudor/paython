62e75fd4a68256822d93fc0daa2ff6a8f42bfe01
boudor <52617103+boudor@users.noreply.github.com>
2019-09-08 17:53:02 +0300

    Add files via upload

---
+++ day20.py
@@ -0,0 +1,14 @@
+thisset = {}
+print(thisset)
+thisset = {"a","h","y"}
+print(thisset)
+y = {"ahmed","bodour","asma","bodour",1,8,8}
+print(y)
+for x in y:
+    print(x)
+    print("bodour" in y)
+    y.add("soso")
+    print(y)
+    u = {"say","hi"}
+    u.update(["welcome"])
+    print(u)
