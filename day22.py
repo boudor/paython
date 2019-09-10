4003da6a0b3fb6db782b5b4af62df4a9a450fc05
boudor <52617103+boudor@users.noreply.github.com>
2019-09-10 23:36:00 +0300

    Add files via upload

---
+++ day22.py
@@ -0,0 +1,25 @@
+thisdict =  {
+"brand":"food",
+"model":"mustange",
+"year" : 1999
+}
+print(thisdict)
+thisdict = {
+"brand":"food",
+"model":"mustange",
+"year" : 1999
+}
+x = thisdict["model"]
+print(x)
+thisdict["year"] = 2000
+print(thisdict)
+c = {
+    "welcome":"mohamed",
+    "sallem" : "bodour",
+}
+for x in c:
+    print(x)
+    for x in c:
+        print(c[x])
+for x , y in c .items():
+    print(x,y)
