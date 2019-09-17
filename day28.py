614befbefb19170f2757e91316186faf34f683a4
boudor <52617103+boudor@users.noreply.github.com>
2019-09-17 10:25:52 +0300

    Add files via upload

---
+++ day28.py
@@ -0,0 +1,22 @@
+i = 1
+while i < 7:
+    print(i)
+    i +=1
+    s = 1
+while s < 6:
+    print(s)
+    if s == 3:
+        break
+    s += 1
+    f = 0
+    while f < 8:
+        f +=1
+        if f ==3:
+            continue
+        print(f)
+        x = 1
+        while x < 4:
+            print(x)
+            x += 1
+        else:
+            print("x is not longer less than 6")
