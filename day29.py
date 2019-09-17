72a60200a604abd0960f0458c3f02f7e53aa1407
boudor <52617103+boudor@users.noreply.github.com>
2019-09-18 00:21:36 +0300

    Add files via upload

---
+++ day29.py
@@ -0,0 +1,16 @@
+fruits = ["apple","banana","cherry"]
+for x in fruits:
+    print(x)
+famliy = ["bobo","shoog","abod"]
+for x in "shoog":
+  print(x)
+  nepghow = ["amoer","rowaf","yzeed"]
+for x in nepghow :
+  if x == "rowaf": 
+   break   
+print(x)
+h = ["mohammed","hsaan","amjad"]
+for x in h:
+    if x == "hsaan":
+        continue
+    print(x)
