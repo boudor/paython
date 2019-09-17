ff7c393fb83fc884f04241c1fa8370b345227833
boudor <52617103+boudor@users.noreply.github.com>
2019-09-17 10:26:40 +0300

    Add files via upload

---
+++ day24.py
@@ -0,0 +1,24 @@
+thisdict = {
+"brand":"ford",
+"model":"mustang",
+"year":1999
+}
+mydict = thisdict.copy()
+print(mydict)
+thifamily = {
+"child1":{
+    "name":"emil",
+    "year":"2004"
+},
+"child2":{
+    "name":"tobias",
+    "year":2007
+},
+"child3":{
+    "name":"linus",
+  "year":1999
+}
+}
+print(thifamily)
+thisdict = dict(brand="food",model = "mustang",year=1998)
+print(thisdict)
