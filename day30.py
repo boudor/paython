fcc3deeea2b54ef6263ad4f89abd71ab8f031e49
boudor <52617103+boudor@users.noreply.github.com>
2019-09-20 03:03:46 +0300

    Add files via upload

---
+++ day30.py
@@ -0,0 +1,11 @@
+for x in range(7) :
+    print(x)
+for x in range(2, 5) :
+    print(x)
+for x in range(2, 30 , 6):
+    print(x)
+adj = ["red","big","tasty"]
+fruits = ["apple","banan","cherry"]
+for x in adj:
+    for y in fruits:
+        print(x, y)
