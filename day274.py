87b656509dbd020b1b7626baea048751da4d844f
boudor <52617103+boudor@users.noreply.github.com>
2019-09-20 03:03:04 +0300

    Add files via upload

---
+++ day274.py
@@ -0,0 +1,19 @@
+a = 66
+b = 77
+if b > a:
+    print("b is greater than a")
+x = 99
+q = 55
+if x > q:
+    print("x is greater than q")   
+elif x == q:
+    print("x and q are equal")
+z = 88
+m = 22
+if m > z:
+    print("m is greater than z")
+elif m == z:
+    print("m and z are equal")
+else:
+    print("z is greater than m")
+    
