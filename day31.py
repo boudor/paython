f9568251d324ab3b553e2a83f46fd92f5e5baf1b
boudor <52617103+boudor@users.noreply.github.com>
2019-09-27 22:08:28 +0300

    Add files via upload

---
+++ day31.py
@@ -0,0 +1,14 @@
+def my_bodour():
+    print("i love bodour more any thing in this world")
+my_bodour()
+def my_fuction(faname):
+    print(faname + "refsnes")
+my_fuction("emil")
+my_fuction("tobias")
+my_fuction("linus")
+def my_fuction(country = "norawy"):
+    print("i am from " + country)
+my_fuction("saui aribi")
+my_fuction("insia")
+my_fuction()
+my_fuction("brazil")
