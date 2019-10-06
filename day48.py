4fd18fdd80cbf8234d05f0b88ce11458d94fa3fd
boudor <52617103+boudor@users.noreply.github.com>
2019-10-06 17:31:15 +0300

    Add files via upload

---
+++ day48.py
@@ -0,0 +1,15 @@
+def myfunc():
+    u = 22
+    print(u)
+myfunc()
+def myfunc():
+    p = 88
+    def myinnerfunc():
+        print(p)
+    myinnerfunc()
+myfunc()
+o = 677
+def myfunc():
+    print(o)
+myfunc()
+print(o)
